25119fb20664d2c524b3b4594b41051ec79d3f3f
RK <khatrimrehan@gmail.com>
2025-12-11 13:09:16 +0530

    hahaha

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
 let changeMode = document.getElementById("changemode")
-
 let lighT = document.getElementById("light")
-
 let loGo = document.getElementById("logo")
-
 let rSetBtn = document.getElementById("reset-btn")
-
 let finGer = document.getElementById("fingeR")
-
 let cliCked = document.getElementById("clicked")
-
 let cliCcked = document.getElementsByClassName("finger-text")
-
-let miniSus = document.getElementById("minuses")
-
+let minUs = document.getElementById("hahaha")
 let incrimenTAnywhere = document.getElementById("incanywhere")
 
 
-const ignore = [rSetBtn, finGer, loGo, lighT, miniSus, cliCked, incrimenTAnywhere];
+const ignore = [rSetBtn, finGer, loGo, lighT, cliCked, minUs, cliCcked, incrimenTAnywhere];
 
 document.addEventListener("click", (e) => {
-    if (ignore.includes(e.target)) {
-        return;
-    }
+  if (ignore.includes(e.target)) {
+    return;
+  }
 });
+
+
+
+function minuses() {
+  if (count > 0) {
+    count -= 1
+  }
+  countEL.textContent = count
+}
+
 
 
 
 function lightmode() {
 
- loGo.src = "sparkel-dark.png"
+  loGo.src = "sparkel-dark.png"
+  rSetBtn.src = "reset-dark.png"
+  finGer.src = "finger-black.svg"
 
- rSetBtn.src = "reset-dark.png"
- 
- finGer.src = "finger-black.svg"
-
- var NAME = document.getElementById("changemode")
- 
- NAME.className="lightmode"
+  var NAME = document.getElementById("changemode")
+  NAME.className = "lightmode"
 }
 
 function darkmode() {
- loGo.src = "fluent--sparkle-16-regular (1).png"
+  loGo.src = "fluent--sparkle-16-regular (1).png"
+  rSetBtn.src = "resetlogo.png"
+  finGer.src =
+    "streamline--one-finger-tap-remix.svg"
 
- rSetBtn.src = "resetlogo.png"
- 
-  finGer.src = 
-"streamline--one-finger-tap-remix.svg" 
- 
- var NAME = document.getElementById("changemode")
- 
- NAME.className="lightmod"
+  var NAME = document.getElementById("changemode")
+  NAME.className = "lightmod"
 }
 
+let count = 0
+if (count > 1) {
+  minUs.style.opacity = "0.8"
+}  
+
+if (count < 1) {
+  minUs.style.opacity = "0.4"
+}  
 
 let countEL = document.getElementById("count-el")
 
-let count = 0
+
 
 
 function incrimentAnywhere() {
@@ -64,17 +68,11 @@ function incrimentAnywhere() {
   countEL.textContent = count
 }
 
-function clicked() {
-  count += 0
-  countEL.textContent = count
-}
-function minuses() {
-  count -= 1
-  countEL.textContent = count
-}
+
 
 function lineoNe() {
-   countEL.textContent = 0
-   count = -1
-
+  countEL.textContent = 0
+  count = -1
 }
+
+
